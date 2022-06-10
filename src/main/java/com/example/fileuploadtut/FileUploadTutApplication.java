913f2df1938cf565e03692c163130e6c3545f8cf
Max <maxotuteye12@gmail.com>
2022-06-10 22:38:54 +0000

    Initial commit

---
+++ src/main/java/com/example/fileuploadtut/FileUploadTutApplication.java
@@ -0,0 +1,13 @@
+package com.example.fileuploadtut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadTutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadTutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fileuploadtut/FileUploadTutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileuploadtut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadTutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
